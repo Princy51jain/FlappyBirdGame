32b829c37c999f75844854c1d5bab976067411b1
Dollcyjain <113534421+Dollcyjain@users.noreply.github.com>
2023-04-01 21:53:29 +0530

    Update FlappyBird(princy).py

--- flappy Bird game/FlappyBird(princy).py
+++ flappy Bird game/FlappyBird(princy).py
@@ -1,8 +1,7 @@
 import random # For generating random numbers
 import sys # We will use sys.exit to exit the program
 import pygame
-from pygame.locals import *
-from zmq import NULL # Basic pygame imports
+from pygame.locals import * # Basic pygame imports
 
 # Global Variables for the game
 FPS = 32
